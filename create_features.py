dd8ed4fba5b32370eb8481469569fed891151c4a
Nathan Maue <nmaue@nyu.edu>
2018-11-28 20:14:38 -0500

    bag of words using up to 300 words

--- create_features.py
+++ create_features.py
@@ -1,7 +1,14 @@
 # pyre-strict
 import json
 from argparse import ArgumentParser
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Tuple, Set
+from nltk.tokenize import wordpunct_tokenize
+from nltk.corpus import stopwords
+from collections import Counter
+
+stopwords = set(stopwords.words('english'))
+
+VOCAB_SIZE: int = 300
 
 
 def get_parser() -> ArgumentParser:
@@ -20,23 +27,57 @@ def inner_main(args) -> None:
     in_file: str = args.in_file
     out_file: str = args.out_file
 
+    cleaned_reviews, vocab = clean_reviews_and_get_vocab(in_file)
+
+    with open(out_file, "w") as outbuffer:
+        for review_dict in cleaned_reviews:
+            output_dict = dict()
+            output_dict["features"] = get_features(
+                review_dict["reviewText"], vocab)
+            output_dict["id"] = review_dict["id"]
+            if "rating" in review_dict:
+                output_dict["rating"] = review_dict["rating"]
+            outbuffer.write(json.dumps(output_dict) + "\n")
+
+
+def clean_reviews_and_get_vocab(
+        in_file: str) -> Tuple[List[Dict[str, Any]], Dict[str, int]]:
+    ret: List[Dict[str, Any]] = []
+    counter: Counter = Counter()
     with open(in_file, "r") as inbuffer:
-        outbuffer = open(out_file, "w")
         for line in inbuffer:
             stripped_line: str = line.strip()
             input_dict: Dict[str, Any] = json.loads(stripped_line)
-            output_dict = dict()
             reviewText: str = input_dict["reviewText"]
-            output_dict["features"] = get_features(reviewText)
-            output_dict["id"] = input_dict["id"]
-            if ("rating" in input_dict):
-                output_dict["rating"] = input_dict["rating"]
-            outbuffer.write(json.dumps(output_dict) + "\n")
-        outbuffer.close()
+            input_dict["reviewText"] = clean_and_tokenize_review(reviewText)
+
+            counter.update(input_dict["reviewText"])
+            ret.append(input_dict)
+
+    elements = counter.most_common(VOCAB_SIZE)
+    ret_vocab = dict()
+    i = 0
+
+    for word in elements:
+        ret_vocab[word[0]] = i
+        i += 1
+
+    return ret, ret_vocab
 
 
-def get_features(review_text: str) -> List[int]:
-    return [0 for x in range(10)]
+def clean_and_tokenize_review(review_text: str) -> List[str]:
+    tokens: List[str] = wordpunct_tokenize(review_text)
+
+    return [token for token in tokens if token not in stopwords]
+
+
+def get_features(review: List[str], vocab: Dict[str, int]) -> List[int]:
+    ret: List[int] = [0 for x in range(len(vocab))]
+    for token in review:
+        if token in vocab:
+            ret[vocab[token]] = 1
+
+    return ret
 
 
 def main() -> None:
