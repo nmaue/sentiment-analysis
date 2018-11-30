f3f4d7a2f448afd098f65142a406975588014fe8
Julie <cestarojulie@gmail.com>
2018-11-29 20:22:23 -0500

    split data into three types of file

--- data_parse.py
+++ data_parse.py
@@ -1,32 +1,74 @@
 import json
+import random
+import copy
 
-output_file = open("cleaned_data.json", "w")
+# output_file = open("cleaned_data.json", "w")
+
+#80%
+training_file = open("training_data.json", "w")
+
+#10%
+development_file_1 = open("development_data_rating_review.json", "w")
+development_file_2 = open("development_data_review.json", "w")
+development_file_3 = open("development_data_rating.json", "w")
+
+#10%
+test_file_1 = open("test_data_review.json", "w")
+test_file_2 = open("test_data_rating.json", "w")
+
 
 input_file = open("small_small_data.json", "r")
 input_file = input_file.read()
 
 split_input_file = input_file.split("\n")
 
-# print(split_input_file)
-# print(len(split_input_file))
-
 things_to_keep = ["reviewText", "overall"]
 
 for i in range(len(split_input_file)):
-# for line in split_input_file:
+	where_it_goes = random.randint(1,11)
+
 	line = split_input_file[i]
 	new_json = json.loads(line)
-	print(new_json)
+	# print(new_json)
 
 	for element in list(new_json):
-		print(element)
+		# print(element)
 		if not element in things_to_keep:
-			print("removed", element)
+			# print("removed", element)
 			new_json.pop(element, None)
 
+	# print(new_json)
+
 	new_json["id"] = i
-	print(new_json)
 
-	json.dump(new_json, output_file)
-	output_file.write("\n")
+	if where_it_goes >= 1 and where_it_goes <= 8:
+		json.dump(new_json, training_file)
+		training_file.write("\n")
+	elif where_it_goes == 9:
+		json.dump(new_json, development_file_1)
+		development_file_1.write("\n")
 
+		new_json_2 = copy.deepcopy(new_json)
+		new_json_2.pop("overall", None)
+		json.dump(new_json_2, development_file_2)
+		development_file_2.write("\n")
+
+		new_json_3 = copy.deepcopy(new_json)
+		new_json_3.pop("reviewText", None)
+		json.dump(new_json_3, development_file_3)
+		development_file_3.write("\n")
+	else:
+		new_json_2 = copy.deepcopy(new_json)
+		new_json_2.pop("overall", None)
+		json.dump(new_json_2, test_file_1)
+		test_file_1.write("\n")
+
+		new_json_3 = copy.deepcopy(new_json)
+		new_json_3.pop("reviewText", None)
+		json.dump(new_json_3, test_file_2)
+		test_file_2.write("\n")
+
+
+
+	# json.dump(new_json, output_file)
+	# output_file.write("\n")
